5ffcc14837a1c23f533a7dc8792c0e4ca649257c
hassan4554 <hm9536057@gmail.com>
2025-08-02 03:34:44 +0500

    added vercel.json

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,2 +1,4 @@
-export const server = "http://localhost:3000/api/v2";
-export const ENDPOINT = "http://localhost:4000/";
+// export const server = "http://localhost:3000/api/v2";
+// export const ENDPOINT = "http://localhost:4000/";
+export const server = "https://buyza-api.onrender.com/api/v2";
+export const ENDPOINT = "https://buyza-socket.onrender.com/";
