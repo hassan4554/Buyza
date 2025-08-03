eb0eb150aa97f9e7e5eef1c0ef6c015855fe38c1
hassan4554 <hm9536057@gmail.com>
2025-08-03 20:23:20 +0500

    added origin

--- backend/config/cors.js
+++ backend/config/cors.js
@@ -1,9 +1,14 @@
-const allowedOrigins = ["http://localhost:5173", "https://buyza.vercel.app/"];
+const allowedOrigins = [
+  "http://localhost:5173/",
+  "https://buyza.vercel.app",
+  "https://ca236dbfdedb.ngrok-free.app",
+];
 
 const corsOptions = {
   origin: (origin, callback) => {
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, origin);
+      return true;
     } else {
       callback(new Error("Not allowed by CORS"));
     }
